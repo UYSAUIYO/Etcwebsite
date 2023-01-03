688f98786b4e65268174d568248474c1b2369bf8
Yu_wen <65168184+UYSAUIYO@users.noreply.github.com>
2023-01-03 09:13:22 +0800

    Add files via upload

---
+++ src/components/About.vue
@@ -0,0 +1,120 @@
+<template>
+    <v-card class="pt-4" :dark="dark" elevation="4" style="background: none;">
+<!--        <v-list-item>-->
+<!--            <v-list-item-content>-->
+<!--                <v-list-item-title class="headline">关于服务器</v-list-item-title>-->
+<!--                <v-list-item-subtitle>About Server</v-list-item-subtitle>-->
+<!--            </v-list-item-content>-->
+<!--        </v-list-item>-->
+        <v-card-text class="f-text" style="max-width: 650px;">
+          <v-card
+              class="mx-auto"
+              max-width="auto"
+          >
+            <v-card
+                dark
+                flat
+            >
+
+              <v-card-title class="pa-2 pink accent-1">
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-title class="headline">关于服务器</v-list-item-title>
+                    <v-list-item-subtitle>About Server</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-card-title>
+
+            </v-card>
+            <v-card-text class="py-0">
+              <v-timeline
+                  align-top
+                  dense
+              >
+                <v-timeline-item
+                    color="pink accent-1"
+                    small
+                >
+                  <v-row class="pt-1">
+                    <v-col cols="4">
+                      <strong>2022-12-1</strong>
+                    </v-col>
+                    <v-col>
+                      <strong>新增问卷系统</strong>
+                      <div class="text-caption">
+                        V1.4.0
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-timeline-item>
+
+                <v-timeline-item
+                    color="pink accent-1"
+                    small
+                >
+                  <v-row class="pt-1">
+                    <v-col cols="4">
+                      <strong>2022-10-31</strong>
+                    </v-col>
+                    <v-col>
+                      <strong>官方网站技术升级</strong>
+                      <div class="text-caption mb-2">
+                        Vuetify Design
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-timeline-item>
+
+                <v-timeline-item
+                    color="pink accent-1"
+                    small
+                >
+                  <v-row class="pt-1">
+                    <v-col cols="4">
+                      <strong>2022-02-11</strong>
+                    </v-col>
+                    <v-col>
+                      <strong>服务器官方网站正式开发</strong>
+                      <div class="text-caption mb-2">
+                        ElementUI
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-timeline-item>
+
+                <v-timeline-item
+                    color="pink accent-1"
+                    small
+                >
+                  <v-row class="pt-1">
+                    <v-col cols="4">
+                      <strong>2021-12-27</strong>
+                    </v-col>
+                    <v-col>
+                      <strong>章鱼蛋正式立项 & BHBing提供技术支持</strong>
+                      <div class="text-caption mb-2">
+                        Maincraft
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-timeline-item>
+              </v-timeline>
+            </v-card-text>
+          </v-card>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        props: {
+            dark: {
+                type: Boolean,
+                required: true,
+            },
+        },
+      data() {
+        return {};
+      }
+    };
+</script>
---
+++ src/components/PickShow.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card class="pt-4" :dark="dark" elevation="4" style="background: none">
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="headline">急招人员</v-list-item-title>
+        <v-list-item-subtitle>Urgently recruit people</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-card-text>
+      目前服务器正在处于高速发展阶段，缺少肝帝大佬和一些开发人员、美术人员等，来作为我们服务器的高速发展做铺垫。
+      <br />由于开发人员开发水平有限，所以<a class="retie">才找一些开发人员的。(别骂了w3w)</a>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    color: "#7d61cd",
+    link: "https://afdian.net/@flyingsky",
+    text: "爱发电",
+  }),
+  props: {
+    dark: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
+<style>
+
+</style>
