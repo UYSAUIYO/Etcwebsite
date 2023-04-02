7912b0499c54c2bd4059e23e24a0ef626fbcc5a2
Yu_wen <65168184+UYSAUIYO@users.noreply.github.com>
2023-04-02 09:40:55 +0800

    Create backup.vue
    重写一部分旧代码给项目的遗留和样式错误。

---
+++ test/backup.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-fab 
+      v-show="showScrollTopButton"
+      color="primary"
+      bottom="15"
+      right="15"
+      small
+      @click="scrollToTop"
+    >
+      <v-icon>mdi-chevron-up</v-icon>
+    </v-fab>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showScrollTopButton: false
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.onScroll);
+  },
+  methods: {
+    onScroll() {
+      this.showScrollTopButton = window.scrollY > 100;
+    },
+    scrollToTop() {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.onScroll);
+  }
+}
+</script>
